e75ac8b4df7950aeeff2b41616a4b5fc24156179
Amulya Jain <amuly.jain@gmail.com>
2024-03-16 18:03:32 -0400

    Update combine_pdfs.py

--- combine_pdfs.py
+++ combine_pdfs.py
@@ -1,16 +1,15 @@
 import os
 import re
 from PyPDF2 import PdfMerger
+
 def sorter(s):
     m = re.compile('[0-9]')
     return m.findall(s)[0]
-def combine_pdfs(pdf_list, output_path):
-    print(pdf_list)
-    merger = PdfMerger()
 
+def combine_pdfs(pdf_list, output_path):
+    merger = PdfMerger()
     for pdf_file in pdf_list:
         merger.append(pdf_file)
-
     merger.write(output_path)
     merger.close()
 
@@ -18,13 +17,8 @@ if __name__ == "__main__":
     # Read the list of PDF files from the generated txt file
     with open("pdf_list.txt", "r") as file:
         pdf_list = [line.strip() for line in file]
-
     # Specify the output path for the combined PDF
     output_path = "AllFormulaSheets.pdf"
     pdf_list.sort(key=sorter)
-    print(pdf_list)
-
     # Combine PDFs
     combine_pdfs(pdf_list, output_path)
-
-    print("PDFs combined successfully.")
